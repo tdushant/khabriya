a3f110d63b006f2fe37a655a16c125b88110d68a
Dushant Sharma <dushant555sharma@gmail.com>
2024-12-02 22:38:10 +0530

    VIdeoplayer  updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
+import Head from "next/head";
 import TVApp from "./components/tv-app";
+import VideoPlayer from "./components/VideoPlayer";
 
 export default function Home() {
 	return (
 		<main>
+			{/* <VideoPlayer/> */}
 			<TVApp />
 		</main>
 	)
