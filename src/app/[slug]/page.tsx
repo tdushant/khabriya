3bd6d3dea5ef8c24d8843bb0b511a1c769af81fd
Mohit Sharma <mohi@mediahubster.com>
2024-12-05 18:12:52 +0530

    fixes update

---
+++ src/app/[slug]/page.tsx
@@ -0,0 +1,11 @@
+import Head from "next/head";
+import TVApp from "../components/tv-app";
+
+export default function Home() {
+	return (
+		<main>
+			{/* <VideoPlayer/> */}
+			<TVApp />
+		</main>
+	)
+}
